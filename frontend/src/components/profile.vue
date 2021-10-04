a78852dfa0446bb77d840b3cae1a1eca9363b656
Kassy729 <hamtol98@gmail.com>
2021-10-04 15:31:29 +0900

    vuetify-layout-setting-commit

---
+++ frontend/src/components/profile.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-img
+        class="profileImg"
+        max-height="270"
+        max-width="270"
+        src="https://picsum.photos/id/11/500/300"
+      ></v-img>
+    </v-row>
+
+    <v-row>
+      <h1>2yubi</h1>
+    </v-row>
+
+    <v-row>
+      <v-btn class="ma-2" outlined color="black">
+        Edit Profile
+      </v-btn>
+    </v-row>
+
+    <v-row>
+      <p><v-icon large color="black"> mdi-account-multiple </v-icon>follower</p>
+      <br />
+      <p>
+        <v-icon large color="black"> mdi-account-multiple </v-icon>following
+      </p>
+    </v-row>
+  </v-container>
+</template>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -3,35 +3,62 @@
     <v-toolbar flat color="11264f" dark>
       <v-toolbar-title>MyStudyApp</v-toolbar-title>
     </v-toolbar>
-    <v-row>
-      <v-col>
-        <v-btn @click="selectedComponent = 'create'">create</v-btn>
-        <v-btn @click="selectedComponent = 'noteList'">List</v-btn>
-        <v-btn @click="selectedComponent = 'test'">test</v-btn>
-        <component v-bind:is="selectedComponent">
-          <p>Default</p>
-        </component>
-      </v-col>
-    </v-row>
+    <v-app-bar align="center">
+      <v-row>
+        <v-col>
+          <v-btn text @click="selectedComponent = 'create'">
+            <v-icon large color="black"> mdi-pencil </v-icon>
+            Note작성</v-btn
+          >
+          <v-btn text @click="selectedComponent = 'noteList'">
+            <v-icon large color="black"> mdi-book-open-variant</v-icon
+            >Note목록보기</v-btn
+          >
+          <v-btn text @click="selectedComponent = 'noteList'">
+            <v-icon large color="black"> mdi-clipboard-edit-outline </v-icon
+            >TodoList</v-btn
+          >
+          <v-btn text @click="selectedComponent = 'test'">test</v-btn>
+          <v-btn text @click="selectedComponent = 'test'">test</v-btn>
+          <v-btn text @click="selectedComponent = 'test'">test</v-btn>
+          <v-btn text @click="selectedComponent = 'test'">test</v-btn>
+        </v-col>
+      </v-row>
+    </v-app-bar>
+    <v-container>
+      <v-row>
+        <v-col cols="auto"> <profile /> </v-col>
+        <v-col><component v-bind:is="selectedComponent"> </component></v-col>
+      </v-row>
+    </v-container>
   </v-card>
 </template>
 
 <script>
 import create from "../components/create.vue";
 import noteList from "../components/noteList.vue";
-import test from "../components/test.vue";
+import profile from "../components/profile.vue";
 
 export default {
   data() {
     return {
       posts: [],
-      selectedComponent: "test",
+      selectedComponent: "create",
     };
   },
   components: {
     create,
     noteList,
-    test,
+    profile,
   },
 };
 </script>
+
+<style>
+.profileImg {
+  width: 300px;
+  height: 300px;
+  border-radius: 70%;
+  overflow: hidden;
+}
+</style>
